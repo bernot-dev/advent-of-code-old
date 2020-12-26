7355b7a66aaa38b6d11a50c404f2dfb9ba3d5d93
Adam Bernot <adam@bernot.dev>
2020-12-26 16:43:00 -0500

    style: rename file variable to f

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -6,8 +6,8 @@ import (
 )
 
 // String returns the contents of the file as a single string
-func String(r *os.File) string {
-	b, err := ioutil.ReadAll(r)
+func String(f *os.File) string {
+	b, err := ioutil.ReadAll(f)
 	if err != nil {
 		panic(err)
 	}
