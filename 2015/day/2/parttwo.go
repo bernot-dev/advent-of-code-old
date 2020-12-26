471dc874e8e8c631091b6b2967d93ef04bea7341
Adam Bernot <adam@bernot.dev>
2020-12-26 18:32:44 -0500

    feat(2015-02): calculate ribbon needed

--- 2015/day/2/parttwo.go
+++ 2015/day/2/parttwo.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"sort"
+)
+
 // PartTwo sovles the second part of the problem.
 func PartTwo(input Input) (solution Solution) {
 	return
+}
+
+// Ribbon determines teh amount of ribbon needed for the box
+func (p *Present) Ribbon() int {
+	dimensions := []int{p.h, p.l, p.w}
+	sort.Sort(sort.IntSlice(dimensions))
+	ribbon := 2 * (dimensions[0] + dimensions[1])
+	bow := p.Volume()
+	return ribbon + bow
+}
+
+// Volume calculates the volume of the box
+func (p *Present) Volume() int {
+	return p.h * p.l * p.w
 }
