9dba7a9a1adc0f0698c93d98a9e47cff011321d3
Adam Bernot <adam@bernot.dev>
2020-12-26 17:05:32 -0500

    feat(helpers): add StringLines

--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -14,6 +15,19 @@ func TestString(t *testing.T) {
 	actual := String(inputFile)
 	expected := "abc"
 	if actual != expected {
+		t.Errorf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestStringLines(t *testing.T) {
+	inputFile, err := os.Open("stringslice.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	defer inputFile.Close()
+	actual := StringLines(inputFile)
+	expected := []string{"a", "b", "c"}
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Expected: %s, Actual: %s", expected, actual)
 	}
 }
