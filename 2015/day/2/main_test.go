3a75aa2e8b2386c3fa6f4ed2a4ea9a329caf721a
Adam Bernot <adam@bernot.dev>
2020-12-26 17:31:54 -0500

    test: add NewPresent

--- 2015/day/2/main_test.go
+++ 2015/day/2/main_test.go
@@ -10,6 +10,20 @@ type TestCase struct {
 	expected  Solution
 }
 
+func TestNewPresent(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected Present
+	}{
+		{"2x3x4", Present{2, 3, 4}},
+		{"1x1x10", Present{1, 1, 10}},
+	}
+	for _, tc := range tests {
+		actual := NewPresent(tc.input)
+		assertDeepEquals(t, actual, tc.expected)
+	}
+}
+
 func assertDeepEquals(t *testing.T, actual, expected interface{}) {
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("Actual: %v\nExpected: %v", actual, expected)
