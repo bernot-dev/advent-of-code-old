3a08e6007d506fe934fd16b07ef76dd0ab67959e
Adam Bernot <adam@bernot.dev>
2020-12-26 15:37:32 -0500

    feat: add string helper

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,1 +1,15 @@
 package helpers
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+// String returns the contents of the file as a single string
+func String(r *os.File) string {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
