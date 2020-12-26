a7bb0fd6d78dc3adcff436e0c4fb9e2b09eb5b7f
Adam Bernot <adam@bernot.dev>
2020-12-26 18:00:25 -0500

    feat: calculate wrapping required

--- 2015/day/2/partone.go
+++ 2015/day/2/partone.go
@@ -1,6 +1,17 @@
 package main
 
+import "github.com/bernot-dev/advent-of-code/helpers"
+
 // PartOne solves the first part of the problem.
 func PartOne(input Input) (solution Solution) {
 	return
+}
+
+// Wrapping determines how much wrapping paper is required for the present
+func (p *Present) Wrapping() int {
+	s1 := p.l * p.w
+	s2 := p.w * p.h
+	s3 := p.h * p.l
+	extra := helpers.Min(s1, s2, s3)
+	return 2*(s1+s2+s3) + extra
 }
