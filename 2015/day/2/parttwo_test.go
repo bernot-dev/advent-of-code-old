fc14aef93da12ae3baf9aa60e5b3fc833d677a2b
Adam Bernot <adam@bernot.dev>
2020-12-26 18:32:44 -0500

    feat(2015-02): calculate ribbon needed

--- 2015/day/2/parttwo_test.go
+++ 2015/day/2/parttwo_test.go
@@ -17,3 +17,35 @@ func TestPartTwo(t *testing.T) {
 		})
 	}
 }
+
+func TestRibbon(t *testing.T) {
+	tests := []struct {
+		input    Present
+		expected int
+	}{
+		{Present{2, 3, 4}, 34},
+		{Present{1, 1, 10}, 14},
+	}
+	for _, tc := range tests {
+		actual := tc.input.Ribbon()
+		if actual != tc.expected {
+			t.Errorf("Expected: %d, Actual: %d", tc.expected, actual)
+		}
+	}
+}
+
+func TestVolume(t *testing.T) {
+	tests := []struct {
+		input    Present
+		expected int
+	}{
+		{Present{2, 3, 4}, 24},
+		{Present{1, 1, 10}, 10},
+	}
+	for _, tc := range tests {
+		actual := tc.input.Volume()
+		if actual != tc.expected {
+			t.Errorf("Expected: %d, Actual: %d", tc.expected, actual)
+		}
+	}
+}
