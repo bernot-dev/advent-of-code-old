4461f9b8557e8f3f4ab1b93a89597790aff995c7
Adam Bernot <adam@bernot.dev>
2020-12-27 10:30:44 -0500

    feat(2015-03): solve part 2

--- 2015/day/3/parttwo.go
+++ 2015/day/3/parttwo.go
@@ -2,5 +2,38 @@ package main
 
 // PartTwo sovles the second part of the problem.
 func PartTwo(input Input) (solution Solution) {
-	return
+	m := make(map[Vector]int)
+	santa := Vector{}
+	robo := Vector{}
+	m[santa]++
+	m[robo]++
+	for i, v := range input {
+		switch i % 2 {
+		case 0:
+			switch v {
+			case '^':
+				santa.Add(N)
+			case '>':
+				santa.Add(E)
+			case 'v':
+				santa.Add(S)
+			case '<':
+				santa.Add(W)
+			}
+			m[santa]++
+		case 1:
+			switch v {
+			case '^':
+				robo.Add(N)
+			case '>':
+				robo.Add(E)
+			case 'v':
+				robo.Add(S)
+			case '<':
+				robo.Add(W)
+			}
+			m[robo]++
+		}
+	}
+	return Solution(len(m))
 }
