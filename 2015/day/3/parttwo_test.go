004e19abc33edf1c813bab374c323051a96f22a5
Adam Bernot <adam@bernot.dev>
2020-12-27 10:30:44 -0500

    feat(2015-03): solve part 2

--- 2015/day/3/parttwo_test.go
+++ 2015/day/3/parttwo_test.go
@@ -5,13 +5,18 @@ import (
 )
 
 func TestPartTwo(t *testing.T) {
-	tests := []TestCase{
-		{"sample.txt", 0},
+	tests := []struct {
+		input    Input
+		expected int
+	}{
+		{"^v", 3},
+		{"^>v<", 3},
+		{"^v^v^v^v^v", 11},
 	}
 	for _, tc := range tests {
-		t.Run(tc.inputFile, func(t *testing.T) {
-			actual := PartTwo(ReadInput(tc.inputFile))
-			if actual != tc.expected {
+		t.Run(string(tc.input), func(t *testing.T) {
+			actual := PartTwo(tc.input)
+			if actual != Solution(tc.expected) {
 				t.Errorf("Expected: %v, Actual: %v", tc.expected, actual)
 			}
 		})
