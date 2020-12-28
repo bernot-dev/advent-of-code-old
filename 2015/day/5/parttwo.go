9ce0caf4ca332650e4aabbc611e59da9f79e10fa
Adam Bernot <adam@bernot.dev>
2020-12-27 20:06:50 -0500

    refactor(2015-05): clean up RepeatedPair function

--- 2015/day/5/parttwo.go
+++ 2015/day/5/parttwo.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"regexp"
-)
-
 // PartTwo sovles the second part of the problem.
 func PartTwo(input Input) (solution Solution) {
 	rules := []Rule{
@@ -21,13 +16,11 @@ func PartTwo(input Input) (solution Solution) {
 
 // RepeatedPair .
 func RepeatedPair(s string) bool {
-	// pairs := make(map[string]bool)
 	for i := 1; i < len(s); i++ {
-		pair := s[i-1 : i+1]
-		pairRegexp := regexp.MustCompile(fmt.Sprintf("(%s)", pair))
-		matches := pairRegexp.FindAllStringIndex(s, -1)
-		if len(matches) > 1 && matches[0][1] <= matches[len(matches)-1][0] {
-			return true
+		for j := i + 2; j < len(s); j++ {
+			if s[i-1] == s[j-1] && s[i] == s[j] {
+				return true
+			}
 		}
 	}
 	return false
