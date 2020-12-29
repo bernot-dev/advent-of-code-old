18519a4d3f742629135a62fa81b4bdefc45cde7c
Adam Bernot <adam@bernot.dev>
2020-12-29 02:02:01 -0500

    feat(2015-07): solve part 2

--- 2015/day/7/parttwo.go
+++ 2015/day/7/parttwo.go
@@ -1,6 +1,20 @@
 package main
 
 // PartTwo sovles the second part of the problem.
-func PartTwo(input Input) (solution Solution) {
-	return
+func PartTwo(input Input, solutionWire string) (solution Solution) {
+	revisedInput := make(Input, 0)
+	for _, ins := range input {
+		if ins.outputWireID == "b" {
+			revisedInput = append(revisedInput, NewGate("16076 -> b"))
+		} else {
+			revisedInput = append(revisedInput, ins)
+		}
+	}
+	c := Circuit{
+		gates: []Gate(revisedInput),
+		wires: nil,
+	}
+	c.CreateWires()
+	c.ConnectGates()
+	return Solution(<-c.wires[solutionWire].c)
 }
--- 2015/day/7/parttwo_test.go
+++ 2015/day/7/parttwo_test.go
@@ -6,11 +6,11 @@ import (
 
 func TestPartTwo(t *testing.T) {
 	tests := []TestCase{
-		{"sample.txt", "a", 0},
+		{"input.txt", "a", 2797},
 	}
 	for _, tc := range tests {
 		t.Run(tc.inputFile, func(t *testing.T) {
-			actual := PartTwo(ReadInput(tc.inputFile))
+			actual := PartTwo(ReadInput(tc.inputFile), tc.solutionWire)
 			if actual != tc.expected {
 				t.Errorf("Expected: %v, Actual: %v", tc.expected, actual)
 			}
