2358ae8f3c3ccc5d134a409790fd42df761e8a77
Adam Bernot <adam@bernot.dev>
2021-08-15 22:51:48 -0400

    add sample data

---
+++ 2015/day/17/go.mod
@@ -0,0 +1,5 @@
+module github.com/bernot-dev/advent-of-code/2015/day/15
+
+go 1.16
+
+require github.com/bernot-dev/advent-of-code/helpers v0.5.0 // indirect
---
+++ 2015/day/17/parttwo_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPartTwo(t *testing.T) {
+	tests := map[string][]TestCase{
+		"sample.txt": {{0}},
+	}
+	for inputFile, ts := range tests {
+		t.Run(inputFile, func(t *testing.T) {
+			input := ReadInput(inputFile)
+			for _, tc := range ts {
+				t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+					actual := PartTwo(input)
+					if actual != tc.expected {
+						t.Errorf("Expected: %v, Actual: %v", tc.expected, actual)
+					}
+				})
+			}
+		})
+	}
+}
